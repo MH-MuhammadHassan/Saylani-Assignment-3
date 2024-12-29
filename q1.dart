dcd99a796a41cc0e298b52bc63d4811307aac542
HassanCodeWave <shforshaikhhassan@gmail.com>
2024-12-29 20:17:48 +0500

    updated with concised way

--- q1.dart
+++ q1.dart
@@ -4,29 +4,20 @@
 import 'dart:io';
 
 void main() {
-  // stdout.write('Enter Number List: use space to separate numbers: ');
-  // List<int> numbers = [];
-  // numbers = stdin.readLineSync()!.split(' ').map(int.parse).toList();
-  // for (var i in numbers) {
-  //   if (i % 2 == 0) {
-  //     print(i);
-  //   }
-  // }
-
-  stdout.write('Enter The Number Which You Want to Add In The List: ');
+  stdout.write('Enter how many numbers you want to add: ');
   int num = int.parse(stdin.readLineSync()!);
   List<int> numbersList = [];
+  List<int> evenNumbersList = [];
+
   for (int i = 1; i <= num; i++) {
     stdout.write('Enter Number $i: ');
     int number = int.parse(stdin.readLineSync()!);
     numbersList.add(number);
+    if (number % 2 == 0) evenNumbersList.add(number);
   }
-  print('User Entered Number List: $numbersList');
-  for (var i in numbersList) {
-    if (i % 2 == 0) {
-      print('Even Number List: $i');
-    } else {
-      print('No Even Number Found');
-    }
-  }
+
+  print('User Entered Numbers: $numbersList');
+  print(evenNumbersList.isEmpty
+      ? 'No even numbers found'
+      : 'Even Numbers: $evenNumbersList');
 }
