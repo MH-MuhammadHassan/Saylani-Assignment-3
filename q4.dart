3ee550bc11b75da9e25fca049f62a04f1b4fef46
HassanCodeWave <shforshaikhhassan@gmail.com>
2024-12-29 21:01:11 +0500

    finds the largest element in a list using a for loop

--- q4.dart
+++ q4.dart
@@ -1,5 +1,9 @@
+// Implement a code that finds the largest element in a list using a for loop.
 void main() {
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  var greatest = numbers.reduce((a, b) => a > b ? a : b);
+  var greatest;
+  for (var i in numbers) {
+    greatest = numbers.reduce((a, b) => a > b ? a : b);
+  }
   print('Greatest Number Of The List is: $greatest');
 }
